fcf9b4ad3938d59c8101c830a23f394a730d3d6c
Rene Weteling <rene@weteling.com>
2026-01-13 13:55:36 +0100

    chore: darkmode enabled everywhere

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// Enforce dark mode by adding 'dark' class to html element
+document.documentElement.classList.add('dark');
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div className="dark">
-      <App />
-    </div>
+    <App />
   </React.StrictMode>,
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "true",
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
